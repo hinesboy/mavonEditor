795edced86801d0ad6b4a8256c677395a73f8bea
yyyybzzzz <z2103740217@qq.com>
2017-06-09 00:25:36 +0800

    增加代码风格可选

--- src/dev/app.vue
+++ src/dev/app.vue
@@ -15,7 +15,7 @@
       <h2 class="item-header">
         {{d_words.default_setting}}
       </h2>
-      <mavon-editor :toolbarsFlag="toolbarsFlag" :editable="editable" :subfield="subfield" :language = "d_language" @change="change" @save="saveone" class="item-editor" v-model="help1"></mavon-editor>
+      <mavon-editor :code_style="code_style" :toolbarsFlag="toolbarsFlag" :editable="editable" :subfield="subfield" :language = "d_language" @change="change" @save="saveone" class="item-editor" v-model="help1"></mavon-editor>
     </div>
     <!--自定义-->
     <div v-if="screen_phone" class="item">
@@ -60,7 +60,8 @@
         },
         subfield: false,
         editable: true,
-        toolbarsFlag: true
+        toolbarsFlag: true,
+        code_style:'code-hybrid'
       }
     },
     created () {
