e97ed8efc6bcfefbc1950a2c38a3e089f284e807
Renaud Dierickx <renaud.dierickx@compte-nickel.fr>
2017-06-23 17:23:27 +0200

    French translation

--- src/lib/config.js
+++ src/lib/config.js
@@ -11,14 +11,18 @@
 
 import help_cn from './lang/cn/help_cn.md'
 import help_en from './lang/en/help_en.md'
+import help_fr from './lang/fr/help_fr.md'
 import words_cn from './lang/cn/words_cn.json'
 import words_en from './lang/en/words_en.json'
+import words_fr from './lang/fr/words_fr.json'
 export const CONFIG = {
     'help_cn': help_cn,
     'help_en': help_en,
+    'help_fr': help_fr,
     'words_cn': words_cn,
     'words_en': words_en,
-    'langList': ['en' , 'cn'],
+    'words_fr': words_fr,
+    'langList': ['en' , 'cn' , 'fr'],
     'toolbars': {
         'bold': true,
         'italic': true,
