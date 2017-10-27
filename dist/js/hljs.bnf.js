31113d0e748025da3f1297016191428f684ca598
hinesboy <hines.zhu@gmail.com>
2017-10-27 11:52:34 +0800

    fix #98,fix #107

--- dist/js/hljs.bnf.js
+++ dist/js/hljs.bnf.js
@@ -1,1 +1,1 @@
-webpackJsonpmavon_editor([157],{316:function(n,e){n.exports=function(n){return{contains:[{className:"attribute",begin:/</,end:/>/},{begin:/::=/,starts:{end:/$/,contains:[{begin:/</,end:/>/},n.C_LINE_COMMENT_MODE,n.C_BLOCK_COMMENT_MODE,n.APOS_STRING_MODE,n.QUOTE_STRING_MODE]}}]}}}});
+webpackJsonpmavon_editor([157],{322:function(n,e){n.exports=function(n){return{contains:[{className:"attribute",begin:/</,end:/>/},{begin:/::=/,starts:{end:/$/,contains:[{begin:/</,end:/>/},n.C_LINE_COMMENT_MODE,n.C_BLOCK_COMMENT_MODE,n.APOS_STRING_MODE,n.QUOTE_STRING_MODE]}}]}}}});
--- dist/js/hljs.julia_repl.js
+++ dist/js/hljs.julia_repl.js
@@ -1,1 +1,1 @@
-webpackJsonpmavon_editor([93],{380:function(a,e){a.exports=function(a){return{contains:[{className:"meta",begin:/^julia>/,relevance:10,starts:{end:/^(?![ ]{6})/,subLanguage:"julia"},aliases:["jldoctest"]}]}}}});
+webpackJsonpmavon_editor([93],{386:function(a,e){a.exports=function(a){return{contains:[{className:"meta",begin:/^julia>/,relevance:10,starts:{end:/^(?![ ]{6})/,subLanguage:"julia"},aliases:["jldoctest"]}]}}}});
