bd08799313bee57d1e94517c3d7c78db5738c6a7
Giuseppe <giuseppe@shiftcode.com.br>
2018-04-17 18:19:52 -0300

    Added pt-BR language

--- src/lib/config.js
+++ src/lib/config.js
@@ -13,17 +13,21 @@
 import help_zh_CN from './lang/zh-CN/help_zh-CN.md'
 import help_en from './lang/en/help_en.md'
 import help_fr from './lang/fr/help_fr.md'
+import help_pt_BR from './lang/fr/help_pt-BR.md'
 import words_zh_CN from './lang/zh-CN/words_zh-CN.json'
 import words_en from './lang/en/words_en.json'
 import words_fr from './lang/fr/words_fr.json'
+import words_pt_BR from './lang/fr/words_pt-BR.json'
 export const CONFIG = {
     'help_zh-CN': help_zh_CN,
+    'help_pt-BR': help_pt_BR,
     'help_en': help_en,
     'help_fr': help_fr,
     'words_zh-CN': words_zh_CN,
+    'words_pt-BR': words_pt_BR,
     'words_en': words_en,
     'words_fr': words_fr,
-    'langList': ['en' , 'zh-CN' , 'fr'],
+    'langList': ['en', 'zh-CN', 'fr', 'pt-BR'],
     'toolbars': {
         'bold': true,
         'italic': true,
