5e542c9f4c458d401a8a867d8ac08719074e1a18
hinesboy <hines.zhu@gmail.com>
2018-07-03 17:27:34 +0800

    config.js import 路径错误

--- src/dev/assets/config.js
+++ src/dev/assets/config.js
@@ -4,13 +4,13 @@
 import help_zh_CN from './lang/zh-CN/help_zh-CN.md'
 import help_en from './lang/en/help_en.md'
 import help_fr from './lang/fr/help_fr.md'
-import help_pt_BR from './lang/fr/help_pt-BR.md'
-import help_ru from './lang/fr/help_ru.md'
+import help_pt_BR from './lang/pt-BR/help_pt-BR.md'
+import help_ru from './lang/ru/help_ru.md'
 import words_zh_CN from './lang/zh-CN/words_zh-CN.json'
 import words_en from './lang/en/words_en.json'
 import words_fr from './lang/fr/words_fr.json'
-import words_pt_BR from './lang/fr/words_pt-BR.json'
-import words_ru from './lang/fr/words_ru.json'
+import words_pt_BR from './lang/pt-BR/words_pt-BR.json'
+import words_ru from './lang/ru/words_ru.json'
 export const CONFIG = {
   'help_zh-CN': help_zh_CN,
   'help_pt-BR': help_pt_BR,
@@ -21,5 +21,5 @@ export const CONFIG = {
   'words_pt-BR': words_pt_BR,
   'words_en': words_en,
   'words_fr': words_fr,
-  'words_ru': words_ru,
+  'words_ru': words_ru
 }
