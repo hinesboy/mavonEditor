e04df708b08a20ffd774c1861b7448eaec69cd38
Rex Tsao <cl0067@126.com>
2019-02-17 16:13:58 +0800

    更新使其支持通过外部css文件来更改编辑器中的markdown样式

--- src/mavon-editor.vue
+++ src/mavon-editor.vue
@@ -314,7 +314,11 @@ export default {
             $vm.initLanguage();
             $vm.iRender();
         })
-        $vm.codeStyleChange($vm.codeStyle, true)
+
+        if (!(typeof $vm.externalLink === 'object'&& typeof $vm.externalLink['markdown_css'] === 'function')) {
+            // 没有外部文件要来接管markdown样式，可以更改markdown样式。
+            $vm.codeStyleChange($vm.codeStyle, true)
+        }
     },
     beforeDestroy() {
         document.body.removeChild(this.$refs.help);
