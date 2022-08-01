8789dd071828b4466a5643478d297451b6d92f77
luozhangbiao <luozhangbiao@360.cn>
2022-08-01 17:05:44 +0800

    fix: Add sanitizer for filtering HTML tags (#744)
    * fix: Add sanitizer for filtering HTML tags
    
    * fix: Do not share `markdown-it` instances
    
    * chore: fix lint
    
    Co-authored-by: wangsongc <wangsongc@foxmail.com>
    (cherry picked from commit b9489a331baf65e856629814f68c6b74f495c863)
    
    # Conflicts:
    #	src/mavon-editor.vue

--- src/mavon-editor.vue
+++ src/mavon-editor.vue
@@ -217,8 +217,6 @@ import md_toolbar_right from './components/md-toolbar-right'
 import autoTextarea from './components/auto-textarea'
 import './lib/font/css/fontello.css'
 import './lib/css/md.css'
-import { skipRule } from './lib/core/rules.js'
-import { FilterXSS } from 'xss'
 
 export default {
 
@@ -318,6 +316,10 @@ export default {
       default() {
         return CONFIG.toolbars
       },
+    },
+    html: {// Enable HTML tags in source
+      type: Boolean,
+      default: true
     },
     xssOptions: {
       // 工具栏
@@ -453,7 +455,6 @@ export default {
       p_external_link: {},
       textarea_selectionEnd: 0,
       textarea_selectionEnds: [0],
-      _xssHandler: null,
     }
   },
 
@@ -881,62 +882,10 @@ export default {
         )
       }
     },
-    xssHandler(htmlCode) {
-      if (this._xssHandler) {
-        return this._xssHandler.process(htmlCode)
-      }
-      let originalTagFun
-      if (typeof this.xssOptions['onTag'] === 'function') {
-        originalTagFun = this.xssOptions['onTag']
-      }
-      this.xssOptions['onTag'] = function(tag, html, info) {
-        let code = skipRule(tag, html)
-        if (originalTagFun) {
-          code = originalTagFun(tag, code)
-        }
-        if (html !== code) {
-          return code
-        }
-      }
-      let originalTagAttr
-      if (typeof this.xssOptions['onTagAttr'] === 'function') {
-        originalTagAttr = this.xssOptions['onTagAttr']
-      }
-
-      this.xssOptions['onTagAttr'] = function(tag, name, value) {
-        const whiteClass = {
-          'div': ['hljs-left', 'hljs-center', 'hljs-right', 'hljs-*'],
-          'code': ['lang-language', 'lang-*'],
-          'span': ['hljs-*'],
-        }
-        let newValue, oriValue
-        if (name === 'class' &&
-          whiteClass[tag] &&
-          whiteClass[tag].find(el => {
-            return !!value.match(el)
-          })) {
-          newValue = name + '="' + value + '"'
-        }
-        if (originalTagAttr) {
-          oriValue = originalTagAttr(tag, name, value)
-        }
-        if (newValue || oriValue) {
-          return oriValue || newValue
-        }
-      }
-
-      this._xssHandler = new FilterXSS(this.xssOptions)
-      return this._xssHandler.process(htmlCode)
-    },
     iRender(toggleChange) {
       var $vm = this
       this.$render($vm.d_value, function(res) {
         // render
-
-        // HTML 渲染前先进行过滤，避免 xss 问题，默认情况下开始此功能
-        if (typeof $vm.xssOptions === 'object') {
-          res = $vm.xssHandler(res)
-        }
 
         $vm.d_render = res
         // change回调  toggleChange == false 时候触发change回调
