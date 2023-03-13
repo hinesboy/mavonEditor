42b6312f201f73f296ce472fa009df42045e2c76
50 <jiawulin@vip.qq.com>
2023-03-13 16:28:37 +0800

    [sync 7b8c872]Reduce the size of the package and do not copy useless files

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -139,8 +139,11 @@ module.exports = {
             from: path.resolve(__dirname, '../resources/markdown'),
             to: path.resolve(__dirname, '../dist/markdown')
         }, {
-            from: path.resolve(__dirname, '../node_modules/katex/dist'),
-            to: path.resolve(__dirname, '../dist/katex')
+            context: 'node_modules/katex/dist',
+            from: {
+                glob: `${path.resolve(__dirname, '../node_modules/katex/dist')}/**/*.+(mjs|min.js|min.css|ttf|woff|woff2)` 
+            },
+            to: path.resolve(__dirname, '../dist/katex')            
         }]),
         new VueLoaderPlugin()
     ]
